5de0962b679693b86015cc0da56ace2dd2244b14
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-08-29 04:17:40 +0300

    Added the a service and Modified it

---
+++ src/app/service/github.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import  {environment} from '../../environments/environment'
+import { User } from '../user';
+import { Repository } from '../repository';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+userCollectedDetails : User;
+userRepositories : Repository;
+
+
+  constructor(private http :HttpClient) {
+    this.userCollectedDetails = new User("","","","",0,0,0);
+    this.userRepositories = new Repository ("","","")
+   }
+
+getUserNameRequest(githubUsername:User){
+ interface ApiResponse{
+    name:string;
+    location:string;
+    bio : string;
+    avatar_url:string;
+    public_repos:number;
+    followers:number;
+    following:number;
+  }
+  let promise = new Promise <void>((resolve,reject)=>{
+    this.http.get<ApiResponse>(environment.apiUrl + githubUsername + '?access_token='+ environment.apiKey ).toPromise().then((response)=>{
+      this.userCollectedDetails= response;
+      resolve();
+    },
+    (error) => {
+      reject(error);
+      console.log (error)
+    }
+    )
+  });
+  return promise
+}
+
+getGithubRepoRequest(githubUsername:any){
+  interface ApiRepositoryResponse{
+    name : string ;
+    html_url : string ;
+    description : string ;
+  
+
+  }
+  let repositoryPromise = new Promise <void> ((resolve,reject)=>{
+    this.http.get<ApiRepositoryResponse>(environment.apiUrl + githubUsername + '/repos?sort=created&direction=desc?access_token=' + environment.apiKey).toPromise().then((response)=>{
+      this.userRepositories = response;
+      resolve();
+    },
+    (error)=>{
+      reject(error)
+      console.log(error)
+    }
+    )  
+  })
+  return repositoryPromise;
+}
+
+}
