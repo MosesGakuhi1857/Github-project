055acd66144ef2d4d07eaec738fbc10a8edb583d
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-08-29 22:19:59 +0300

    Modified the file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchUsernameComponent } from './search-username/search-username.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search-username', component: SearchUsernameComponent},
+  { path: 'home-page', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
