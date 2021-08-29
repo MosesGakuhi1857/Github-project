dcf6fe992a64c1fc613cac0b1996ed9ba61ede47
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-08-30 02:08:27 +0300

    Modified the file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import {NgProgressHttpClientModule} from '@ngx-progressbar/http-client';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchUsernameComponent } from './search-username/search-username.component'
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SearchUsernameComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
