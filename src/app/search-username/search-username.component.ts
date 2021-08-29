f9248b5fc3d5420f26158f226452437b86b49800
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-08-29 20:34:08 +0300

    Modified the file

---
+++ src/app/search-username/search-username.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { GithubService } from '../service/github.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-username',
+  templateUrl: './search-username.component.html',
+  styleUrls: ['./search-username.component.css']
+})
+export class SearchUsernameComponent implements OnInit {
+    
+  data:any;
+  repositories:any;
+  searchTerm: string;
+  displayUserDetailContainer = false;
+  displayGithubUserErrorNotFound = false;
+  displayUserRepositoryList = false;
+  displayUserErrorMessage = false;
+
+  // accessing the form inputs
+  @ViewChild('searchRef') searchForm: NgForm;
+
+  constructor(private userservice :GithubService ) { }
+
+  
+
+  ngOnInit(): void {}
+      // search github user
+  searchGithubUser(){
+    console.log(this.searchTerm)
+    this.userservice.getUserNameRequest(this.searchTerm).then(
+      (response) => {
+          this.data = this.userservice.userCollectedDetails;
+          this.displayUserDetailContainer = true;
+      },
+      (error) => {
+        console.log(error);
+        this.displayGithubUserErrorNotFound = true;
+      }
+    );
+
+  
+  // this.searchTerm = this.searchForm.value.search;
+  this.userservice.getGithubRepoRequest(this.searchTerm).then(
+    (response) => {
+      this.repositories = this.userservice.userRepositories;
+      this.displayUserRepositoryList = true;
+    },
+    (error) => {
+      this.displayUserErrorMessage = true;
+      console.log(error);
+    }
+  ); // end of searchGithubUserRepositories
+  }
+  
+}
